524e0ceb2f4264c28848be962b28b10a6d1141de
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-06-24 12:34:07 +0200

    feat: add first reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 )
